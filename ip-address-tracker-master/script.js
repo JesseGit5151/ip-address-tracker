4f887f91d44b21fa433d21667b3b2196def68ddf
JesseGit5151 <jrodriguez5151yahoo.com>
2021-11-08 15:40:27 -0800

    css styles finished
    finished header & main section styles. Added leaflet JS map.

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -29,3 +29,11 @@ let gatherData = async (url) => {
                         </div>`;
 };
 gatherData(apiURL);
+
+var map = L.map('map', {
+  center: [51.505, -0.09],
+  zoom: 13
+});
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
